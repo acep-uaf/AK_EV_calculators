3dd101b14c9eb6cfbb7b4ceab9813427ca90da71
Michelle Wilber <46725544+mmwilber@users.noreply.github.com>
2023-07-15 17:39:51 -0800

    Update EV_Emissions.py

--- EV_Emissions.py
+++ EV_Emissions.py
@@ -458,7 +458,9 @@ fig, ax = plt.subplots()
 #ax.bar(x,tmy_month.kwh, width=0.35, align='edge', label = 'EV')
 ax.bar(x,tmy_month.parke, width=0.35, align='edge', label = 'Parked')
 ax.bar(x,tmy_month.drivee, width=0.35, align='edge', bottom = tmy_month.parke, label = 'Driving')
-ax.bar(x,tmy_month.idlee, width=0.35, align='edge', bottom = [tmy_month.drive + tmy_month.parke], label = 'Idling')
+#because I suck at matplotlib...:
+tmy_month['dp'] = tmy_month.drive + tmy_month.parke
+ax.bar(x,tmy_month.idlee, width=0.35, align='edge', bottom = tmy_month.dp, label = 'Idling')
     
 # Add the axis labels
 ax.set_xlabel('Month')
